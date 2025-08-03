e554762cdfbdc08af5c3520fcce66f61442cf20a
Rene DeLage <renedelage86@gmail.com>
2025-08-03 17:04:14 -0500

    Update Victoria SQL Analysis.sql

--- Victoria SQL Analysis.sql
+++ Victoria SQL Analysis.sql
@@ -1,12 +1,15 @@
 -- Create and select new database
 CREATE SCHEMA redata;
 USE redata;
+
 -- Imported table data, now checking data looks accurate
 SELECT * FROM victoria;
 -- Dropping timestamp from Dates
 ALTER TABLE Victoria MODIFY COLUMN SoldDate DATE, MODIFY COLUMN ListDate DATE;
+
 -- Checking results
 SELECT * FROM Victoria;
+
 -- Total sales by year, from highest to lowest
 SELECT YEAR(SoldDate) "Year", SUM(SoldPrice) Volume FROM Victoria GROUP BY YEAR(SoldDate) ORDER BY Volume DESC;
 -- Running totals of sales for all active sales years
@@ -18,6 +21,7 @@ SELECT SoldDate, SoldPrice, SUM(SoldPrice) OVER(ORDER BY SoldDate, Agency) AS Ru
 SELECT SoldDate, SoldPrice, SUM(SoldPrice) OVER(ORDER BY SoldDate, Agency) AS RunningTotal FROM Victoria WHERE YEAR(SoldDate) = 2022;
 SELECT SoldDate, SoldPrice, SUM(SoldPrice) OVER(ORDER BY SoldDate, Agency) AS RunningTotal FROM Victoria WHERE YEAR(SoldDate) = 2023;
 SELECT SoldDate, SoldPrice, SUM(SoldPrice) OVER(ORDER BY SoldDate, Agency) AS RunningTotal FROM Victoria WHERE YEAR(SoldDate) = 2025;
+
 -- Total Sales for each city
 SELECT City, SUM(soldprice) Volume FROM Victoria GROUP BY City ORDER BY Volume DESC;
 -- Total Sales for each city by active sales year
@@ -29,9 +33,12 @@ SELECT City, SUM(soldprice) Volume FROM Victoria WHERE YEAR(SoldDate) = 2021 GRO
 SELECT City, SUM(soldprice) Volume FROM Victoria WHERE YEAR(SoldDate) = 2022 GROUP BY City ORDER BY Volume DESC;
 SELECT City, SUM(soldprice) Volume FROM Victoria WHERE YEAR(SoldDate) = 2023 GROUP BY City ORDER BY Volume DESC;
 SELECT City, SUM(soldprice) Volume FROM Victoria WHERE YEAR(SoldDate) = 2025 GROUP BY City ORDER BY Volume DESC;
+
 -- Stats by Year
 SELECT YEAR(SoldDate) "Year", Agency, SUM(SoldPrice) Volume, SUM(Units) Units, ROUND(AVG(SoldPrice), 0) "Average Transaction" FROM Victoria GROUP By YEAR(SoldDate), Agency ORDER BY Year(SoldDate);
+
 -- Total Stats
 SELECT SUM(SoldPrice) Volume, SUM(Units) Units, ROUND(AVG(SoldPrice), 0) AS "Avg Transaction", ROUND(AVG(DOM), 0) as "Avg DOM" FROM Victoria;
+
 -- Total Stats by Agency
 SELECT Agency, SUM(SoldPrice) Volume, SUM(Units) Units, ROUND(AVG(SoldPrice), 0) AS "Avg Transaction", ROUND(AVG(DOM), 0) as "Avg DOM" FROM Victoria GROUP BY Agency ORDER BY Volume DESC;
